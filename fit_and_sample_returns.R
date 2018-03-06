2a05446856175c93b23c71811568ea3d21093cf0
MICHELLE ZHANG <mzhang8@stanford.edu>
2018-03-05 16:35:19 -0800

    Add latest things

--- fit_and_sample_returns.R
+++ fit_and_sample_returns.R
@@ -1,10 +1,16 @@
-#library(fitdistrplus)
+library(readxl)
 library(quantmod)
 library(mvtnorm)
 library(MASS)
 
-# clear workspace
+set.seed(1)
+
+# setup environment
+setwd("/Users/michellezhang/Desktop/endowment_fund")
 rm(list = ls())
+
+# indexes_data.xlsx provided
+index_data = read_excel("indexes_data.xlsx")
 
 # stocks we're using
 stocks = c("^GSPC")
@@ -18,6 +24,7 @@ alternative = c()
 # cash
 cash = c()
 
+# all assets
 assets = c(stocks, bonds, alternative, cash)
 
 # gets the return of an asset denoted by its symbol from Yahoo Finance
@@ -25,32 +32,42 @@ get_return <- function(symbol, from_date, to_date) {
   s = getSymbols(symbol, from=from_date, auto.assign = F)
   s_adj_close = data.frame(s)[,6]
   
-  periodReturn(s, period='yearly',from=from_date, to=to_state)  # change to monthly, if desired, later
+  periodReturn(s, period='monthly',from=from_date, to=to_state)  
 }
 
+# define params
 returns = c()
 from_date = "2000-01-01"
 to_date = "2018-01-01"
+num_scenarios = 5
 
-# iterate through all assets and do following:
-# 1. get returns
-# 2. add to vector of returns
-# then:
-# 3. fit to multivariate distribution
-# 4. print parameters
+# build up vector of returns from assets
 for (i in 1:length(assets)) { 
   asset <- assets[i]
   return <- as.numeric(get_return(asset, from_date, to_date))
-  returns <- rbind(returns, return)
+  returns <- cbind(returns, return)
 }
 
+# take log of returns
 log_returns <- log(returns + 1)
 df <- data.frame(log_returns)
-row.names(df) <- assets
+colnames(df) <- assets
 
-means = rowMeans(df)
-sd = diag(sqrt(cov(t(df))))
-corrs = cor(t(df))
+# fit distribution
+df.mu = colMeans(df)
+df.Sigma = cov(df)
+df.corrs = cor(df)
+
+# convert to yearly params
+df.mu = 12 * df.mu
+df.Sigma = 12 * df.Sigma
+df.sd = diag(sqrt(df.Sigma))
+
+# output into file
+
+############################################
+##        IGNORE BELOW FOR NOW            ##
+############################################
 
 # use qq-plot to check for goodness-of-fit
 qqnorm(returns)
