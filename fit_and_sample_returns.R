7be4c291f3f079029946e99b9e2ae2fc9c839ef8
MICHELLE ZHANG <mzhang8@stanford.edu>
2018-03-06 00:49:36 -0800

    Use actual data for fitting

--- fit_and_sample_returns.R
+++ fit_and_sample_returns.R
@@ -1,9 +1,5 @@
 library(readxl)
 library(quantmod)
-library(mvtnorm)
-library(MASS)
-
-set.seed(1)
 
 # setup environment
 setwd("/Users/michellezhang/Desktop/endowment_fund")
@@ -11,34 +7,42 @@ rm(list = ls())
 
 # indexes_data.xlsx provided
 index_data = read_excel("indexes_data.xlsx")
+hedge_data = read_excel("hedgefund.xlsx")
+am_hedge_data = read_excel("hedgefund_amurica.xlsx")
+
+hedge_data = hedge_data[100:221, 2]
+am_hedge_data = am_hedge_data[100:221, 2]
+
+hedge_data = as.numeric(unlist(hedge_data)) / 100
+am_hedge_data = as.numeric(unlist(am_hedge_data)) / 100
 
 # stocks we're using
 stocks = c("^GSPC")
 
 # bonds we're using
-bonds = c("QQQ")
+bonds = c("LQD", "IEF")
 
 # alternative investments
 alternative = c()
 
 # cash
-cash = c()
+cash = c("BIL")
 
 # all assets
 assets = c(stocks, bonds, alternative, cash)
 
 # gets the return of an asset denoted by its symbol from Yahoo Finance
 get_return <- function(symbol, from_date, to_date) {
-  s = getSymbols(symbol, from=from_date, auto.assign = F)
+  s = getSymbols(symbol, from=from_date, to_date=to_date, auto.assign = F)
   s_adj_close = data.frame(s)[,6]
   
-  periodReturn(s, period='monthly',from=from_date, to=to_state)  
+  periodReturn(s, period='monthly',from=from_date, to=to_date)  
 }
 
 # define params
 returns = c()
-from_date = "2000-01-01"
-to_date = "2018-01-01"
+from_date = "2008-01-01"
+to_date = "2018-03-01"
 
 # build up vector of returns from assets
 for (i in 1:length(assets)) { 
@@ -47,9 +51,15 @@ for (i in 1:length(assets)) {
   returns <- cbind(returns, return)
 }
 
+# add hedge fund index
+returns <- returns[1:122, 1:4]
+returns <- cbind(returns, hedge_data)
+
+
 # take log of returns
 log_returns <- log(returns + 1)
 df <- data.frame(log_returns)
+assets = c(assets, "HEDGE")
 colnames(df) <- assets
 
 # fit distribution
@@ -59,11 +69,15 @@ df.corrs = cor(df)
 
 # convert to yearly params
 df.mu = 12 * df.mu
+df.mu
 df.Sigma = 12 * df.Sigma
-df.sd = diag(sqrt(df.Sigma))
+df.Sigma
+df.sd = sqrt(diag(df.Sigma))
+df.sd
 
 # compute cholesky fact
 df.chol = chol(df.Sigma)
+df.chol
 
 # output into file
 
@@ -82,14 +96,14 @@ library('ggplot2')
 library('forecast')
 library('tseries')
 
-sp500 <- as.numeric(get_return("^GSPC", from_date, to_date))
-sp500_train <-sp500[1:(0.9 * length(sp500))] 
-sp500_test <- sp500[(0.9 * length(sp500) + 1):length(sp500)] 
+#sp500 <- as.numeric(get_return("^GSPC", from_date, to_date))
+#sp500_train <-sp500[1:(0.9 * length(sp500))] 
+#sp500_test <- sp500[(0.9 * length(sp500) + 1):length(sp500)] 
 
-ar.fit <- arima(sp500_train, order = c(2, 0, 0))
-ar.preds <- predict(ar.fit, n.ahead = (length(sp500) - (0.9 * length(sp500))))$pred
-ar.forecast <- forecast(ar.fit, h = 100)
+#ar.fit <- arima(sp500_train, order = c(2, 0, 0))
+#ar.preds <- predict(ar.fit, n.ahead = (length(sp500) - (0.9 * length(sp500))))$pred
+#ar.forecast <- forecast(ar.fit, h = 100)
 
-plot(ar.forecast, main = "AR forecasts for asset returns")
+#plot(ar.forecast, main = "AR forecasts for asset returns")
 
-accuracy(ar.preds, sp500_test)[2] 
+#accuracy(ar.preds, sp500_test)[2] 
